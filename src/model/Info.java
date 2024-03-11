37f0b8b5bb02f92d0c26db74decbe6a8f3b6292b
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-03-11 10:04:55 +0600

    created one model class.

---
+++ src/model/Info.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Info {
+    public Info(String name, String city, String university, double cgpa) {
+        this.name = name;
+        this.city = city;
+        this.university = university;
+        this.cgpa = cgpa;
+    }
+
+    private String name;
+
+    private String city;
+    private String university;
+    private double cgpa;
+}
